d6091296b1610c957cfa9f9076746667f7d76353
xxfalpid <xxfalpidd99@gmail.com>
2026-01-23 19:56:54 +1000

    Написали класс Дробь

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print("Я очень люблю бургеры")
+class Fraction:
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+
+    def set_numerator(self, numerator): #устанавливаем новое значение числителя
+        self.numerator = numerator
+
+
+    def set_denominator(self, denominator): #устанавливаем новое значение знаменателя
+        self.denominator = denominator
+
+
+    def get_numerator(self): # получаеММММММММММ значение числителя
+        return self.numerator
+
+
+    def get_denominator(self): # получаеМММ значение знаменателя
+        return self.denominator
+
+
+    def display_output(self):
+        print(f'{self.numerator}/{self.denominator}')
+
+
+    def sum_fraction(self, fraction67):
+        denominators = self.denominator * denominator.fraction67
+        
+
+
+
+
+
+
+
+
+
+
+
+fraction1 = Fraction(6,7)
